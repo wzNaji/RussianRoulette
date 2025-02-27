a6b3311e4ef55d438fba8c59ec3ad6d6cbea223e
wzNaji <walid.z.naji@gmail.com>
2025-02-27 20:53:13 +0100

    updated port and start criteria for heorku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
-app.use(express.static('.'));  // Tager alle filer i mit dir
+// Process.env.PORT must be used for Heroku to bind the port correctly
+const port = process.env.PORT || 3000;  // Fallback to 3000 if process.env.PORT is not defined
+
+app.use(express.static('.'));  // Serve all files in the current directory
 
 app.listen(port, () => {
-    console.log("det funrer")
+    console.log(`Server is running on port ${port}`);
 });
